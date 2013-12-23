45f02cd630082045432888a4a449642e4e84fb91
Rafael Nieto <rnietoy@gmail.com>
2013-12-22 16:43:13 -0800

    Add master layout page. Add empty/placeholder Home and Account controller classes.

---
+++ NietoYostenMvc/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NietoYostenMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            ViewBag.Message = HttpContext.User.Identity.IsAuthenticated
+                ? "You are authenticated"
+                : "You are not authenticated";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Members()
+        {
+            return View();
+        }
+    }
+}
