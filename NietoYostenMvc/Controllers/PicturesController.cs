170bb03938aaa1f76020a91f965f404aec8bfaea
Rafael Nieto <rnietoy@gmail.com>
2014-02-22 20:48:00 -0800

    Initial picture upload code. Changed routing for pictures controller.

--- NietoYostenMvc/Controllers/PicturesController.cs
+++ NietoYostenMvc/Controllers/PicturesController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using Elmah;
 using Massive;
 using NietoYostenMvc.Code;
+using NietoYostenMvc.Models;
 
 namespace NietoYostenMvc.Controllers
 {
@@ -13,12 +17,12 @@ namespace NietoYostenMvc.Controllers
     {
         public const int AlbumPageSize = 20;
 
-        private DynamicModel _pictures;
+        private Pictures _pictures;
         private DynamicModel _albums;
 
         public PicturesController()
         {
-            _pictures = new DynamicModel("NietoYostenDb", "Pictures");
+            _pictures = new Pictures();
             _albums = new DynamicModel("NietoYostenDb", "Albums");
         }
 
@@ -83,18 +87,20 @@ namespace NietoYostenMvc.Controllers
         }
 
         [RequireLogin]
-        public ActionResult ShowPicture(string album, int picture)
+        public ActionResult ShowPicture(int pictureid)
         {
             dynamic model = new ExpandoObject();
 
             // Get picture details, including row number (to be used to calculate the album page)
             var query = _pictures.Query(
                 "SELECT * FROM " +
-                "  (SELECT ROW_NUMBER() OVER (ORDER BY P.ID) AS Row, P.ID, P.Title, P.FileName, A.FolderName FROM Pictures P " +
+                "  (SELECT ROW_NUMBER() OVER (ORDER BY P.ID) AS Row, P.ID, P.Title, P.FileName, A.FolderName " +
+                "  FROM Pictures P " +
                 "  INNER JOIN Albums A ON A.ID = P.AlbumID " +
-                "  WHERE A.FolderName = @0) T " +
-                "WHERE ID = @1",
-                album, picture);
+                "  WHERE A.FolderName = (SELECT FolderName FROM Albums WHERE ID = P.AlbumID) " +
+                "  ) T " +
+                "WHERE ID = @0",
+                pictureid);
 
             model.Picture = query.FirstOrDefault();
 
@@ -108,20 +114,147 @@ namespace NietoYostenMvc.Controllers
                 "SELECT TOP 1 P.ID FROM Pictures P " +
                 "INNER JOIN Albums A ON A.ID = P.AlbumID " +
                 "WHERE P.ID < @0 ORDER BY P.ID DESC",
-                picture);
+                pictureid);
 
             // Get ID of next picture in album
             model.NextID = _pictures.Scalar(
                 "SELECT TOP 1 P.ID FROM Pictures P " +
                 "INNER JOIN Albums A ON A.ID = P.AlbumID " +
                 "WHERE P.ID > @0 ORDER BY P.ID",
-                picture);
+                pictureid);
 
             // Calculate page of requested picture in album
             long page = ((model.Picture.Row - 1)/AlbumPageSize) + 1;
             model.AlbumPage = page;
 
             return View((object)model);
+        }
+
+        /// <summary>
+        /// Upload pictures to an album
+        /// </summary>
+        /// <param name="id">Album folder name</param>
+        /// <returns></returns>
+        [RequireRole(Role = "family")]
+        public ActionResult Upload(string id)
+        {
+            return View();
+        }
+
+        private class UploadPictureDto
+        {
+            public string fileName;
+            public string folderName;
+            public long position;
+            public string errorMsg = null;
+        }
+
+        public string GetTempFilePath(string fileName)
+        {
+            return Path.Combine(
+                HttpContext.Server.MapPath("~/content/pictures/upload"),
+                fileName + ".incomplete");
+        }
+
+        [HttpPost]
+        [RequireRole(Role = "family")]
+        public ActionResult BeginFileUpload(string folderName, string fileName, string base64Data)
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                return HttpNotFound();
+            }
+
+            long position;
+            using (FileStream fs = System.IO.File.Create(GetTempFilePath(fileName)))
+            {
+                byte[] data = Convert.FromBase64String(base64Data);
+                fs.Write(data, 0, data.Length);
+                position = fs.Position;
+            }
+
+            return Json(new UploadPictureDto
+            {
+                fileName = fileName,
+                folderName = folderName,
+                position = position
+            });
+        }
+
+        [HttpPost]
+        [RequireRole(Role = "family")]
+        public ActionResult UploadFileChunk(string folderName, string fileName, string base64Data)
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                return HttpNotFound();
+            }
+
+            long position = 0;
+            // Try several times, sometimes the file has not been completely closed since the
+            // last call to this method, so we get a "file is opened by another process" exception.
+            // We wait for a little bit and then try again.
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    using (FileStream fs = System.IO.File.Open(GetTempFilePath(fileName), FileMode.Append))
+                    {
+                        byte[] data = Convert.FromBase64String(base64Data);
+                        fs.Write(data, 0, data.Length);
+                        position = fs.Position;
+                    }
+                    break;
+                }
+                catch (System.IO.IOException ex)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(ex);
+                    Thread.Sleep(200);
+                }
+            }
+
+            return Json(
+                new UploadPictureDto
+                {
+                    fileName = fileName,
+                    folderName = folderName,
+                    position = position
+                });
+        }
+
+        [HttpPost]
+        [RequireRole(Role = "family")]
+        public ActionResult EndFileUpload(string folderName, string fileName, string base64Data)
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                return HttpNotFound();
+            }
+
+            string originalPicFile = Path.Combine(
+                HttpContext.Server.MapPath("~/content/pictures/original/" + folderName),
+                fileName);
+
+            try
+            {
+                System.IO.File.Copy(GetTempFilePath(fileName), originalPicFile);
+            }
+            catch (IOException)
+            {
+                return Json("Error: Un archivo con este nombre ya existe en este álbum.");
+            }
+            finally
+            {
+                if (System.IO.File.Exists(GetTempFilePath(fileName)))
+                {
+                    System.IO.File.Delete(GetTempFilePath(fileName));    
+                }
+            }
+
+            // Add picture to database
+            _pictures.Add(folderName, fileName);
+
+            return Json("La imagen se subió con éxito");
         }
     }
 }
