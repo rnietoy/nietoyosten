3db221b6ede529cab3d1ae6ef807ae063014842b
Rafael Nieto <rnietoy@gmail.com>
2014-07-19 16:05:46 -0700

    draft of article admin page

---
+++ NietoYostenMvc/Controllers/ArticleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NietoYostenMvc.Code;
+using NietoYostenMvc.Models;
+
+namespace NietoYostenMvc.Controllers
+{
+    public class ArticleController : ApplicationController
+    {
+        private Articles _articles;
+
+        public ArticleController()
+        {
+            _articles = new Articles();
+        }
+
+        [RequireLogin]
+        public ActionResult Index(string id)
+        {
+            IEnumerable<dynamic> articles = _articles.GetArticlesBySection(id);
+            return View("Index", articles);
+        }
+
+    }
+}
